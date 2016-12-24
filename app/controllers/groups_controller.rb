d6cb0375d8d465d1810d3544f201fa15b07d5e5b
YuyaUsami <yu0524.engineer@gmail.com>
2016-12-24 18:33:51 +0900

    Add_code-by-collection_check_boxes
    groups_userモデルを作成し、groups_usersテーブルを作成した。
    collection_check_boxesを使って、チャットメンバーのviewを作成した。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,25 +1,5 @@
 class GroupsController < ApplicationController
-  class Post
-    include ActiveModel::Model
-
-    attr_accessor :persons
-  end
-
-  class Person
-    include ActiveModel::Model
-
-    attr_accessor :a_name
-  end
-
-  class Group
-    include ActiveModel::Model
-
-    attr_accessor :name
-  end
-
   def new
-    persons = [Person.new(a_name: '宇佐見祐也'), Person.new(a_name: '宇佐見祐也')]
-    @person = Post.new(persons: persons)
     @group = Group.new
   end
 
