306eb08f5d87b80f7a92e5796cb573462c5e3181
YuyaUsami <yu0524.engineer@gmail.com>
2016-12-26 17:22:32 +0900

    Correct_file-byPullrequest
    編集機能のルーティングとコントローラの削除、改行、フラッシュメッセージ機能の編集を行った。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,18 +6,12 @@ class GroupsController < ApplicationController
   def create
     @group = Group.create(create_params)
     if @group.save
+      flash[:success] = '作成に成功しました。'
       redirect_to controller: 'messages' , action: 'index'
     else
       flash[:false] = '作成に失敗しました。'
-      redirect_to controller: 'groups' ,action: 'new'
+      render 'new'
     end
-  end
-
-  def edit
-    @group = Group.find(params[:id])
-  end
-
-  def update
   end
 
   private
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
   def index
-    flash[:success] = '作成に成功しました。'
   end
 end
