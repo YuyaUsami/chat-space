6b986fff331ed1a0040e5f6d067dfe422f981e41
YuyaUsami <yu0524.engineer@gmail.com>
2016-12-29 22:17:58 +0900

    edit_message-controller
    messageコントローラーを条件分岐をつけて編集した。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,24 @@
 class MessagesController < ApplicationController
   def index
-    @group = Group.find(1)
+    @group = Group.find(params[:group_id])
     @message = Message.new
-    @messages = Message.order("created_at DESC")
+    @messages = Message.where(group_id: params[:group_id])
   end
 
   def create
-    Message.create(comment_params)
-    redirect_to group_messages_path
+    @group = Group.find(params[:group_id])
+    @message = Message.new(comment_params)
+    if @message.save
+      flash[:success] = '作成に成功しました。'
+      redirect_to group_messages_path(@group)
+    else
+      flash[:false] = '作成に失敗しました。'
+      render 'index'
+    end
   end
 
   private
   def comment_params
-    params.require(:message).permit(:body).merge(group_id: params[:group_id])
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
