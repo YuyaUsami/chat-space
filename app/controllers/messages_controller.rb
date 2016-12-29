3bc97e4d94af8922d9e221838ed2c61f6feb2c0a
YuyaUsami <yu0524.engineer@gmail.com>
2016-12-29 16:31:31 +0900

    Add_controller-create
    コントローラーを追加した。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,16 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
     @messages = Message.order("created_at DESC")
+  end
+
+  def create
+    Message.create(comment_params)
+    redirect_to message_path
+  end
+
+  private
+  def comment_params
+    params.require(:message).permit(:body).merge(group_id: params[:group_id])
   end
 end
