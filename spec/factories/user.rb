0d5945067b4b8da6199072629cf5b0abf37d1576
YuyaUsami <yu0524.engineer@gmail.com>
2017-01-11 16:53:49 +0900

    Add_user_factories
    factory_girlを用いてuserのインスタンスを生成させるコードを記述した。

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "yuya"
+    email                 "yuya@gmail.com"
+    password              "aaaaaaaa"
+    password_confirmation "aaaaaaaa"
+  end
+end
