09c8864090a48d80640a95a304eadee77066b60d
YuyaUsami <yu0524.engineer@gmail.com>
2017-01-12 15:38:07 +0900

    introduce_faker
    fakerを用いて、記述を行った。

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name                  "yuya"
-    email                 "yuya@gmail.com"
+    name                  { Faker::Name.name }
+    email                 { Faker::Internet.email }
     password              "aaaaaaaa"
     password_confirmation "aaaaaaaa"
   end
