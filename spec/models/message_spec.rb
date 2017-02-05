8c4734db954ba88337488f55010bcb141581d987
YuyaUsami <yu0524.engineer@gmail.com>
2017-02-05 16:50:03 +0900

    change_model/message_spec.rb
    message_spec.rbの記述を変更した。

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
+    it "is valid with a body" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
     it "is invalid without an body" do
-      user = build(:message)
+      user = build(:message, :with_body)
       user.valid?
       expect(user.errors[:body]).to include("を入力してください。")
     end
