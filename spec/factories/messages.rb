f3a506f138ef87634e1832b33a3c8b8e8cffc30e
YuyaUsami <yu0524.engineer@gmail.com>
2017-02-05 16:48:57 +0900

    Add_factory
    factoryの記述を追加した。

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :message do
-    body ""
+    body "aaaaa"
+    image nil
+    group_id nil
+    user_id nil
     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
+
+    trait :with_body do
+      body nil
+    end
   end
 end
