582b5df798a2e57de617834c5e6fbba4bdd5df64
YuyaUsami <yu0524.engineer@gmail.com>
2017-02-10 10:18:10 +0900

    delete_code
    現在は不要である記述を削除した。

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :message do
     body { Faker::Lorem.sentence }
-    image { Faker::Avatar.image }
     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
 
     trait :with_empty_body do
