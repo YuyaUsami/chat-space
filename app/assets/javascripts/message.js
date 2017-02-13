dae11b10b9546566a20a7e8930958e04c6685706
YuyaUsami <yu0524.engineer@gmail.com>
2017-02-13 20:58:03 +0900

    modify_message.js-byPullrequest
    空白、変数jsonUrlをなくした。またjsが動くかのテストで使っていたconsole.logの記述を削除した。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,23 +3,17 @@ $(function() {
     var html = "<div class = 'chat-main-message-title'>" + "<p class = 'chat-main-message-title__name' >" + message.name + "</p>" +"<p class =  'chat-main-message-title__time' >" + message.time + "</p>" + "</div>" +"<p class = 'chat-main-message__message'>" + message.body + "</p>";
     return html;
   }
-
   $('.js-form').on('submit', function(e) {
-    console.log('ajax')
     e.preventDefault();
     var textField = $('.input_message');
     var message = textField
-    var jsonUrl = window.location.href;
-
     $.ajax({
       type: 'POST',
-      url: jsonUrl,
+      url: window.location.href,
       data: message,
       dataType: 'json'
     })
-
     .done(function(data) {
-      console.log("hogehoge");
       var html = buildHTML(data);
       $('.chat-main-message').append(html);
       textField.val('');
